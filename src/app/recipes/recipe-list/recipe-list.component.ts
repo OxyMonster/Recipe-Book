bf599887e1daebc7b17c604e69a5aac2ec41d5a2
Oxymonster <giorgizeikidze97@gmail.com>
2019-06-14 17:01:36 +0400

    Book-Recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Description of Test Rercipe','https://www.wellplated.com/wp-content/uploads/2018/04/Mediterranean-Pasta-recipe-600x792.jpg')
+  ];
+  
   constructor() { }
 
   ngOnInit() {
